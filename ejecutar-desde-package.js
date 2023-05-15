979141718a0ea2e38e3e3d01fb17a28caa3a70e0
BVEGAlab <100711306+BVEGAlab@users.noreply.github.com>
2023-05-15 16:44:16 -0500

    Udemy: Using Timezone moment

--- ejecutar-desde-package.js
+++ ejecutar-desde-package.js
@@ -1,1 +1,20 @@
-console.log('Hola Mundo'); 
+const moment = require('moment-timezone');
+
+const identity = 'cv-dt1-20230515-1'
+
+const year = identity.split('-')[2].substring(0, 4);
+const month = identity.split('-')[2].substring(4, 6);
+const day = identity.split('-')[2].substring(6, 8);
+
+
+const currentDate = moment.tz('America/Bogota');
+const angelesDate = moment.tz('America/Mexico_City');
+const finalRloDate = moment.utc([+year, +month - 1, +day, 12, 0, 0, 0]).toISOString();
+
+console.log("Hora de Bogota: ",currentDate)
+console.log("Hora de Mexico: ",angelesDate)
+console.log("Hora del Release: ", finalRloDate)
+
+if (currentDate.isSameOrBefore(finalRloDate, 'day')) {
+    console.log('El dia de Bogota corresponde al dia del Release')
+} 
